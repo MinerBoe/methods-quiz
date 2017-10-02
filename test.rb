7deffcf581c4fcc398c056f0c74dd9a493c969a0
Mats Dale <14minerboe16@gmail.com>
2017-10-02 10:39:01 -0600

    Tests for has_teen?

--- test.rb
+++ test.rb
@@ -2,4 +2,30 @@ require './test_setup'
 require './methods_quiz'
 
 # TODO - write tests here
+describe 'methods_quiz' do
 
+	describe 'has_teen?' do
+
+		it 'all are teens return true' do
+			has_teen?(15, 15, 15).must_equal(true)
+		end
+
+		it 'no teens return false' do
+			has_teen?(12, 20, 100).must_equal(false)
+		end
+
+		it 'one teen return true' do
+			has_teen?(15, 20, 12).must_equal(true)
+		end
+
+		it 'one teen is first endpoint return true' do
+			has_teen?(13, 20, 20).must_equal(true)
+		end
+
+		it 'one teen is last endpoint return true' do
+			has_teen?(20, 19, 20).must_equal(true)
+		end
+
+	end
+
+end
